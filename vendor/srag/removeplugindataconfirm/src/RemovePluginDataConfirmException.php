479d40922ae0d20a67703a0c6a6ef8c470b3d82d
Florian <fw@studer-raimann.ch>
2018-10-08 15:06:34 +0200

    Supports latest DICTrait

--- vendor/srag/removeplugindataconfirm/src/RemovePluginDataConfirmException.php
+++ vendor/srag/removeplugindataconfirm/src/RemovePluginDataConfirmException.php
@@ -8,6 +8,8 @@ use ilException;
  * Class RemovePluginDataConfirmException
  *
  * @package srag\RemovePluginDataConfirm
+ *
+ * @author  studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
  */
 class RemovePluginDataConfirmException extends ilException {
 
