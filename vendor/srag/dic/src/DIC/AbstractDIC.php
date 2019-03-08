ab59865b7b5b683c6de1377cbcc5db9f3e0b218d
Florian Wyss <fw@studer-raimann.ch>
2019-03-08 12:43:28 +0100

    Rename plugin

---
+++ vendor/srag/dic/src/DIC/AbstractDIC.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace srag\DIC\CrsMemberGalleryRoleColor\DIC;
+
+/**
+ * Class AbstractDIC
+ *
+ * @package srag\DIC\CrsMemberGalleryRoleColor\DIC
+ *
+ * @author  studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
+ */
+abstract class AbstractDIC implements DICInterface {
+
+	/**
+	 * AbstractDIC constructor
+	 */
+	protected function __construct() {
+
+	}
+}
