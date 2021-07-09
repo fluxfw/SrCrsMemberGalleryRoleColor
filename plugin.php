69f2fb8fdbcbe90a0c8c419527779038ae7c7cbc
fluxfw <fluxfw@fluxlabs.ch>
2021-07-09 14:53:58 +0200

    ILIAS 7 support

--- plugin.php
+++ plugin.php
@@ -6,6 +6,6 @@ require_once __DIR__ . "/vendor/autoload.php";
 $id = "srcrsmgrc";
 $version = "1.3.0";
 $ilias_min_version = "6.0";
-$ilias_max_version = "6.999";
+$ilias_max_version = "7.999";
 $responsible = "fluxlabs";
 $responsible_mail = "support@fluxlabs.ch";
--- vendor/srag/librarylanguageinstaller/README.md
+++ vendor/srag/librarylanguageinstaller/README.md
@@ -48,5 +48,5 @@ use srag\LibraryLanguageInstaller\SrCrsMemberGalleryRoleColor\x\LibraryLanguageI
 
 ## Requirements
 
-* ILIAS 6.0 - 6.999
+* ILIAS 6.0 - 7.999
 * PHP >=7.2
